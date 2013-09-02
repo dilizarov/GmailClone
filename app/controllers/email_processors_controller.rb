c8ef015aade89c0aa2e3f76915a9d07e9d9f2bdd
David Ilizarov <dilizarov@gmail.com>
2013-09-01 20:14:20 -0700

    Email works?

--- app/controllers/email_processors_controller.rb
+++ app/controllers/email_processors_controller.rb
@@ -1,8 +1,15 @@
 class EmailProcessorsController < ApplicationController
 
   def create
-    p params[:email]
-    Email.create(params[:email])
+    
+    @user = params[:recipient]
+    @user = @user[0...-24]
+    @user += '@gmaily.com'
+    @user = User.find_by_email(@user);
+    
+    Email.create(recipient_id: @user, sender_address: params[:from], title: params[:subject],
+                 content: params['body-mime'], starred: false,
+                 read: false, parent_email_id: -1);
   end
 
 end
