431c620252e4fae502eb2adca71d774d66098d7b
David Ilizarov <dilizarov@gmail.com>
2013-09-01 21:00:21 -0700

    fixed error

--- app/controllers/email_processors_controller.rb
+++ app/controllers/email_processors_controller.rb
@@ -1,20 +1,22 @@
-class EmailProcessorsController < ApplicationController
+class EmailProcessorController < ApplicationController
 
   def create
     
     user = params[:recipient]
-    user = user[0...-24]
-    user +='@gmaily.com'
-    user = ser.find_by_email(@user);
-    user = user.id
+    # user = user[0...-24]
+    # user +='@gmaily.com'
+    # user = ser.find_by_email(@user);
+    # user = user.id
+    
+    user = 1
     
     Email.create(recipient_id: user, sender_address: params[:from], title: params[:subject],
                  content: params['body-plain'], starred: false,
-                 read: false, parent_email_id: -1);
+                 read: false, parent_email_id: -1)
                  
-    # Email.create(recipient_id: 1, sender_address: "failure@failure.com", title: "That failed",
-#     content: "This is a message telling that it failed!", starred: false, read: false, parent_email_id: -1);             
-#                  
+    Email.create(recipient_id: 1, sender_address: "failure@failure.com", title: "That failed",
+    content: "This is a message telling that it failed!", starred: false, read: false, parent_email_id: -1);             
+                  
     head :ok
   end
 
