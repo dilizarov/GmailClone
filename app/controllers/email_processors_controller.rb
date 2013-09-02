e9e9466d9594834da90b2de2192249d19cc034ed
David Ilizarov <dilizarov@gmail.com>
2013-09-01 21:03:22 -0700

    Finally fixed, I think...

--- app/controllers/email_processors_controller.rb
+++ app/controllers/email_processors_controller.rb
@@ -3,12 +3,10 @@ class EmailProcessorController < ApplicationController
   def create
     
     user = params[:recipient]
-    # user = user[0...-24]
-    # user +='@gmaily.com'
-    # user = ser.find_by_email(@user);
-    # user = user.id
-    
-    user = 1
+    user = user[0...-24]
+    user +='@gmaily.com'
+    user = ser.find_by_email(@user);
+    user = user.id
     
     Email.create(recipient_id: user, sender_address: params[:from], title: params[:subject],
                  content: params['body-plain'], starred: false,
