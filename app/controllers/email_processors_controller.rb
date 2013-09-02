673d1c93f93c4a80fd8951c66dbb5819d9424f3e
David Ilizarov <dilizarov@gmail.com>
2013-09-01 20:55:15 -0700

    omgomgomg

--- app/controllers/email_processors_controller.rb
+++ app/controllers/email_processors_controller.rb
@@ -8,7 +8,7 @@ class EmailProcessorController < ApplicationController
     user = ser.find_by_email(@user);
     user = user.id
     
-    Email.create(recipient_id: @user, sender_address: params[:from], title: params[:subject],
+    Email.create(recipient_id: user, sender_address: params[:from], title: params[:subject],
                  content: params['body-plain'], starred: false,
                  read: false, parent_email_id: -1);
                  
